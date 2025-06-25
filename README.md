6d6a4b38bce4d3e49b189ca905a2681187587b08
gabriellucasknp <glss@cesar.school>
2025-06-25 14:46:26 -0300

    Update and rename README.md to metaativa.py

--- README.md
+++
@@ -1,2 +0,0 @@
-# metaativa
-protótipo de uma rede social que tem o objetivo de conectar as pessoas através dos exercícios físicos.
---
+++ metaativa.py
@@ -0,0 +1,2 @@
+# metaativa
+#protótipo de uma rede social que tem o objetivo de conectar as pessoas através dos exercícios físicos.
