e19ed1bccd5aedd4cbce4f3018888218b45ea08a
gabriellucasknp <glss@cesar.school>
2026-01-27 18:16:43 -0300

    Delete app_metaativa directory

--- app_metaativa/tests.py
+++
@@ -1,6 +0,0 @@
-from django.test import TestCase
-
-class MetaativaTestCase(TestCase):
-    def test_basic_functionality(self):
-        """Teste básico para verificar se tudo funciona"""
-        pass
