8d4806a3a5d1b331719de62b9aa8a354c417f47f
gabriellucasknp <glss@cesar.school>
2026-01-27 00:37:37 -0300

    Delete apps.py

--- apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppMetaativaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'app_metaativa'
-    verbose_name = 'Metaativa'
